3edfaa2ac345a0721fe8b7e53d1979da05b29281
PedroNnsM <pedronunes95@outlook.com>
2023-05-23 23:20:36 -0300

    Seed user admin criado

---
+++ API/src/database/knex/seeds/users.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("users").del();
+  await knex("users").insert([
+    {
+      id: 1,
+      name: "Sou Admin",
+      email: "admin@email.com",
+      password: "$2a$08$qAaN2Odtk1sOPwS1b9lP6euhH7qW.ps1BYRIaMdOGsZ7XeGch0pky",
+      role: "admin",
+    },
+  ]);
+};
