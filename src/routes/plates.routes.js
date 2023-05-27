4d0e45ebe9f7f48fcee9e444d196c894b4b15fd2
PedroNnsM <pedronunes95@outlook.com>
2023-05-27 14:56:47 -0300

    iniciando a criação do controller e routes starting controller and router Createplates + image upload and diskstorage

---
+++ src/routes/plates.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+
+const PlatesController = require("../controllers/PlatesController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const platesRoutes = Router();
+
+const platesController = new PlatesController();
+
+platesRoutes.use(ensureAuthenticated);
+
+platesRoutes.get("/", platesController.index);
+platesRoutes.post("/", platesController.create);
+platesRoutes.get("/:id", platesController.show);
+platesRoutes.delete("/:id", platesController.delete);
+
+module.exports = platesRoutes;
