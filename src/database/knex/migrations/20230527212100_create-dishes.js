d198a0764826fdcd23159b08f63ad73bed3af6b3
PedroNnsM <pedronunes95@outlook.com>
2023-06-10 21:39:17 -0300

    fix: migrate dishes

--- src/database/knex/migrations/20230527212100_create-dishes.js
+++ src/database/knex/migrations/20230527212100_create-dishes.js
@@ -1,11 +1,12 @@
 exports.up = (knex) =>
   knex.schema.createTable("dishes", (table) => {
     table.increments("id");
+    table.integer("user_id").references("id").inTable("users");
+    table.text("image").default(null);
     table.text("title").notNullable();
     table.text("description").notNullable();
     table.float("price").notNullable();
     table.enu("category", ["meal", "drinks", "dessert"]).notNullable();
-    table.text("img").notNullable();
     table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
     table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
   });
