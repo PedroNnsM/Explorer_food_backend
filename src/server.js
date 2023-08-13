6ea0ca7b8c6e1f2238bddcc7a260393026b96969
PedroNnsM <pedronunes95@outlook.com>
2023-08-13 00:37:13 -0300

    get images

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ require("dotenv/config");
 // const uploadConfig = require("./configs/upload");
 
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
 
 const cors = require("cors");
 const express = require("express");
@@ -14,7 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
+
+ app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
