657db328753409c406aa585abb55ce8e8872b408
PedroNnsM <pedronunes95@outlook.com>
2023-08-13 00:25:52 -0300

    update image

--- src/controllers/ImagesController.js
+++ src/controllers/ImagesController.js
@@ -10,10 +10,28 @@ class ImagesController {
       throw new AppError("Nenhuma foto selecionada");
     }
     const diskStorage = new DiskStorage();
-console.log(image)
+    console.log(image);
     const filename = await diskStorage.saveFile(image.filename);
 
-    return response.json({filename});
+    return response.json({ filename });
+  }
+
+  async update(request, response) {
+    const diskStorage = new DiskStorage();
+    const dishe_id = request.dishe.id;
+    const imageFilename = request.file.filename;
+
+    const dishe = await knex("dishes");
+
+    if (dishe.image) {
+      await diskStorage.deleteFile(dishe.image);
+    }
+    const filename = diskStorage.saveFile(imageFilename);
+    dishe.image = filename;
+
+    await knex("dishes").update(dishe).where({ id: dishe_id });
+
+    return response.json(dishe);
   }
 }
 module.exports = ImagesController;
