eef8b7bec4752f68c1c43668f96b980cfc19e1bf
PedroNnsM <pedronunes95@outlook.com>
2023-10-26 16:20:18 -0600

    Refactory

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -4,13 +4,25 @@ const uploadConfig = require("../configs/upload");
 
 class DiskStorage {
   async saveFile(file) {
-    console.log('oi')
+    console.log("oi");
     await fs.promises.rename(
       path.resolve(uploadConfig.TMP_FOLDER, file),
       path.resolve(uploadConfig.UPLOADS_FOLDER, file)
     );
 
     return file;
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
+
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return
+    }
+
+    await fs.promises.unlink(filePath)
   }
 }
 
